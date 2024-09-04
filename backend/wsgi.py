d5ebdf51713a544b6b4886775558ee8387767856
Shrut Vanpariya <shrutvanpariya@gmail.com>
2024-09-04 22:11:41 +0530

    backend update

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,3 +1,10 @@
 from src.app import app 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PORT = os.getenv('PORT')
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=(PORT if PORT is not None else 5000), debug=True)
